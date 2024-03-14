c77a3c0a08dbcdaaed1013844ee7258fc7e2c855
Emre Basboga <emre.basboga@tuni.fi>
2024-03-14 23:43:37 +0200

    Fixed bots hand generation, made more functions

--- RCP/main.cpp
+++ RCP/main.cpp
@@ -4,114 +4,161 @@
 #include <set>
 #include <cctype>
 
+
 using namespace std;
 
 string WIN = "> You win! New game?: Yes (Y) No (N)";
 string LOSE = "> You lost... Rematch?: Yes (Y) No (N)";
+string TIE = "> It's a tie! Rematch?: Yes (Y) No (N)";
 string WRONG_INPUT = "Incorrect input";
 
-bool input_cheker(char input){
+void to_lower(string& input){
 
-    input = tolower(input);
 
-    set<char> inputs = {'r', 's', 'p', 'y', 'n'};
-    if (inputs.find(input) == inputs.end() ){
-        return false;
+    for (char& i : input){
+        i = tolower(i);
     }
-    return true;
+
 }
 
-char generate_random_hand(){
+void get_results(string player, string bot){
+
+    //Cheking if the player has won
+    if ((player == "p" && bot == "r") ||
+        (player == "s" && bot == "p") ||
+        (player == "r" && bot == "s") ){
+
+        cout << WIN << endl << "> ";
+    }
+
+    //Cheking if the results are a tie
+    else if ((player == "p" && bot == "p") ||
+            (player == "s" && bot == "s") ||
+             (player == "r" && bot == "r") ){
+
+        cout << TIE << endl << "> ";
+    }
+
+    //The player has lost
+    else{
+        cout << LOSE << endl << "> ";
+    }
+
+}
 
 
+bool input_cheker(string input, int type){
+
+    to_lower(input);
+
+    set<string> inputs = {"r", "s", "p"};
+    set<string> rematch_inputs = {"y", "n"};
+
+    if (type == 1){
+        if (inputs.find(input) == inputs.end() ){
+            return false;
+        }
+        return true;
+    }
+    else{
+        if (rematch_inputs.find(input) == inputs.end() ){
+            return false;
+        }
+        else{
+            //cout << input << " chekkaa tää" << endl;
+            return true;
+        }
 
 
-    //Generating the ranfom number for deciding the opponents hand
+    }
+
+
+}
+
+
+string generate_random_hand() {
     srand(time(NULL));
     int max = 3;
     int min = 1;
 
-    int number;
-    number = rand() % (max - min + 1) + min;
+    int number = rand() % (max - min + 1) + min;
 
-
-
-
-    //Choosing the right path to return the correct hand
-    if (number == 1){
-        return 'r';
+    if (number == 1) {
+        return "r";
     }
-    else if (number == 2){
-        return 'p';
+    else if (number == 2) {
+        return "p";
     }
-    else{
-        return 'p';
+    else {
+        return "s";
     }
-
-
 }
+
+string get_players_choice(){
+    while (true){
+
+        //Players hand
+        string hand;
+
+        cout << "> ";
+        getline(cin, hand);
+        cout << endl;
+
+        if (!input_cheker(hand, 1)){
+            cout << WRONG_INPUT << endl << endl << "> ";
+        }
+        else{
+            return hand;
+        }
+    }
+}
+
+bool ask_rematch(){
+
+    while (bool flag = true){
+
+        string rematch;
+        getline(cin, rematch);
+        cout << endl;
+
+        if (input_cheker(rematch, 2)){
+            if (rematch == "n"){
+                cout << "> See you later fighter.";
+                return false;
+            }
+            else if(rematch == "y"){
+                return true;
+            }
+        }
+        else{
+            cout << WRONG_INPUT << endl << endl << "> ";
+        }
+    }
+    return true;
+}
+
 
 int main()
 {
 
     while (true){
 
+        cin.clear();
         cout << "> Choose your hand Rock (R), Paper (P), Scissors (S): " << endl;
 
-        char hand;
 
 
 
-        while (true){
+        string players_hand = get_players_choice();
+        string bot_hand = generate_random_hand();
 
+        get_results(players_hand, bot_hand);
 
-            cout << "> ";
-            cin >> hand;
-            cout << endl;
-
-            if (!input_cheker(hand)){
-                cout << WRONG_INPUT << endl << endl << "> ";
-            }
-            else{
-                break;
-            }
-        }
-
-        char opponent_hand;
-        opponent_hand = generate_random_hand();
-
-
-        if ((hand == 'p' && opponent_hand == 'r') || (hand == 's' && opponent_hand == 'p')
-            || (hand == 'r' && opponent_hand == 's')){
-
-            cout << WIN << endl << "> ";
-
-        }
-
-        else{
-            cout << LOSE << endl << "> ";
+        if (!ask_rematch()){
+            return 0;
         }
 
 
 
-        while (bool flag = true){
-
-            char rematch;
-            cin >> rematch;
-            cout << endl;
-
-            if (input_cheker(rematch)){
-                if (rematch == 'n'){
-                    cout << "> See you later fighter.";
-                    return 0;
-                }
-                else if(rematch == 'y'){
-                    break;
-                }
-            }
-            else{
-                cout << WRONG_INPUT << endl << endl << "> ";
-            }
-        }
     }
 }
--- testi/main.cpp
+++ testi/main.cpp
@@ -8,45 +8,14 @@ using namespace std;
 
 int main()
 {
-    while (true){
-        string next;
-        cin >> next;
+    int* dyn_muuttujan_osoite = nullptr;
+    dyn_muuttujan_osoite = new int(7);
+    cout << "Osoite: " << dyn_muuttujan_osoite << endl;
+    cout << "Alku:   " << *dyn_muuttujan_osoite << endl;
+    *dyn_muuttujan_osoite = *dyn_muuttujan_osoite * 4;
+    cout << "Loppu:  " << *dyn_muuttujan_osoite << endl;
+    delete dyn_muuttujan_osoite;
 
-        srand(time(NULL));
-
-        int max = 3;
-        int min = 1;
-
-        int random;
-        random = rand() % (max - min + 1) + min;
-
-
-        cout << random << endl;
-        }
-    return 0;
-}
-
-/*
-#include <iostream>
-#include <cstdlib> // For rand() and RAND_MAX
-#include <ctime>   // For time()
-
-using namespace std;
-
-// Function to generate a random integer within [min, max]
-int generate_random_number(int min, int max) {
-    // Generate a random number within the range [0, max-min], then add min
-    return rand() % (max - min + 1) + min;
-}
-
-int main() {
-    // Seed the random number generator with the current time
-    srand(time(NULL));
-
-    // Generate a random number within the range [1, 3]
-    int randomNumber = generate_random_number(1, 3);
-    cout << "Random number: " << randomNumber << endl;
 
     return 0;
 }
-*/
